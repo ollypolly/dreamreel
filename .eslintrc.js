58a740a584b889b052b68b20f1d03572c9749a5a
Olly Pethick <ollypethick@gmail.com>
2023-01-17 01:32:45 +0000

    Add rule to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,8 @@ module.exports = {
     // I suggest this setting for requiring return types on functions only where useful
     "@typescript-eslint/explicit-function-return-type": "off",
 
+    "@typescript-eslint/strict-boolean-expressions": "warn",
+
     // Includes .prettierrc.js rules
     "prettier/prettier": [
       "warn",
